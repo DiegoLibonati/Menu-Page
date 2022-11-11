51b8f37fe9bc8332a6f9a31ffce1c1e654481317
DiegoLibonati <diego.libonati1998@gmail.com>
2022-11-10 21:01:35 -0300

    New readme, fix code js, new html semantic, css fix

--- README.md
+++ README.md
@@ -17,56 +17,6 @@
 
 I made a web page that allows you to see a food menu. This menu has different buttons and depending on which button we touch it will bring us that type of food. If we tap on All, it will bring up all the meals again. I made this page with POO.
 
-## Feel free to edit my code
-
-If you want to add more attributes to the foods.
-
-```
-constructor(nombre, precio, descripcion, img){
-    this.nombre = nombre;
-    this.precio = precio;
-    this.descripcion = descripcion;
-    this.img = img;
-}
-```
-
-Html food.
-
-```
-insertarInfo(){
-    return `<article class="section_container_article-item">
-
-    <div class="item-imagen">
-    <img src="${this.img}" alt="${this.nombre}">
-    </div>
-
-    <div class="section_container_article-informacion">
-        <div class="item-informacion">
-        <h3>${this.nombre}</h3>
-        <h3>${this.precio}</h3>
-        </div>
-
-        <div class="item-descripcion">
-            <p>${this.descripcion}</p>
-        </div>
-    </div>
-
-</article>`
-}
-```
-
-If you want to create a food
-
-```
-const breakfastCuatro = new Comida("Smoky beans & baked eggs", "$30", "Pack in the nutrients with smoky beans and baked eggs. Great for a veggie family lunch or supper, serve with flatbreads or toast for extra sustenance", "YOUR IMAGE LINK");
-```
-
-Before you create a new food, you must add that food to the respective list
-
-```
-let breakfastDesayunos = [breakfastUno, breakfastDos,breakfastTres,breakfastCuatro];
-```
-
 ## Technologies used
 
 1. Javascript
@@ -89,6 +39,125 @@ let breakfastDesayunos = [breakfastUno, breakfastDos,breakfastTres,breakfastCuat
 
 ## Video
 
-
 https://user-images.githubusercontent.com/99032604/199139166-84b8a1cc-c6ad-424a-90e0-ee2c2bcb3696.mp4
 
+## Documentation
+
+In `sectionCenter` we get where we are going to render the meals, then we get all the buttons in the other variables which would be `all, breakfast, lunch and shakes`:
+
+```
+const sectionCenter = document.querySelector(".section_container_article");
+const btnAll = document.getElementById("all");
+const btnBreakfast = document.getElementById("breakfast");
+const btnLunch = document.getElementById("lunch");
+const btnShakes = document.getElementById("shakes");
+```
+
+The class `Food` will have as attributes `name, amount, description, img` and a single method called `insertInformation()` this method will be in charge of printing the HTML with the information that we pass to it by the attributes:
+
+```
+class Food {
+  constructor(name, amount, description, img) {
+    this.name = name;
+    this.amount = amount;
+    this.description = description;
+    this.img = img;
+  }
+
+  insertInformation() {
+    return `<div class="section_container_article-item">
+
+        <div class="item-imagen">
+            <img src="${this.img}" alt="${this.name}">
+        </div>
+
+        <div class="section_container_article-informacion">
+            <div class="item-informacion">
+            <h3>${this.name}</h3>
+            <h3>${this.amount}</h3>
+            </div>
+
+            <div class="item-descripcion">
+                <p>${this.description}</p>
+            </div>
+        </div>
+
+    </div>`;
+  }
+}
+```
+
+In this case we will take as an example `lunchs` this will be our array of this specific category in which we created two types of food that are lunch, we created 2 objects of the class `Food` and they were instantiated:
+
+```
+const lunchOne = new Food(
+  "Big-batch bolognese",
+  "$15",
+  "Whip up a huge batch of bolognese that's fit to feed a hungry crowd, or freeze half for a speedy midweek meal",
+  "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1074456_10-1a5351d.jpg?quality=90&webp=true&resize=300,272"
+);
+const lunchTwo = new Food(
+  "Meal prep: pasta",
+  "$15",
+  "Make three lunchbox pasta meals in one go to save you time midweek. They're nutritious and healthy with variations using salmon, chicken and aubergine",
+  "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/mealprep-pasta-merged_web3_copy-8687106.jpg?quality=90&webp=true&resize=300,272"
+);
+
+const lunchs = [lunchOne, lunchTwo];
+```
+
+In this array we will store all the meals to render all the meals when we touch the `All` button:
+
+```
+const allFoods = [
+  breakfastOne,
+  breakfastTwo,
+  breakfastTr,
+  breakfastFour,
+  lunchOne,
+  lunchTwo,
+  shakesOne,
+  shakesTwo,
+];
+```
+
+When the DOM is fully loaded all the meals will be displayed and then each button will be assigned a click event that will allow it to render the meals of its category:
+
+```
+window.addEventListener("DOMContentLoaded", () => {
+  let displayMenu = allFoods.map(function (item) {
+    return item.insertInformation();
+  });
+
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+
+  btnAll.addEventListener("click", () => {
+    sectionCenter.innerHTML = displayMenu;
+  });
+
+  btnBreakfast.addEventListener("click", () => {
+    let displayMenuBreakfast = breakfasts.map(function (bf) {
+      return bf.insertInformation();
+    });
+    displayMenuBreakfast = displayMenuBreakfast.join("");
+    sectionCenter.innerHTML = displayMenuBreakfast;
+  });
+
+  btnLunch.addEventListener("click", () => {
+    let displayMenuLunch = lunchs.map(function (lunch) {
+      return lunch.insertInformation();
+    });
+    displayMenuLunch = displayMenuLunch.join("");
+    sectionCenter.innerHTML = displayMenuLunch;
+  });
+
+  btnShakes.addEventListener("click", () => {
+    let displayMenuShakes = shakes.map(function (shakes) {
+      return shakes.insertInformation();
+    });
+    displayMenuShakes = displayMenuShakes.join("");
+    sectionCenter.innerHTML = displayMenuShakes;
+  });
+});
+```
--- codigo.js
+++ codigo.js
@@ -4,93 +4,137 @@ const btnBreakfast = document.getElementById("breakfast");
 const btnLunch = document.getElementById("lunch");
 const btnShakes = document.getElementById("shakes");
 
-class Comida{
-    constructor(nombre, precio, descripcion, img){
-        this.nombre = nombre;
-        this.precio = precio;
-        this.descripcion = descripcion;
-        this.img = img;
-    }
+class Food {
+  constructor(name, amount, description, img) {
+    this.name = name;
+    this.amount = amount;
+    this.description = description;
+    this.img = img;
+  }
 
-    insertarInfo(){
-     return `<article class="section_container_article-item">
+  insertInformation() {
+    return `<div class="section_container_article-item">
                 
         <div class="item-imagen">
-        <img src="${this.img}" alt="${this.nombre}">
+            <img src="${this.img}" alt="${this.name}">
         </div>
 
         <div class="section_container_article-informacion">
             <div class="item-informacion">
-            <h3>${this.nombre}</h3>
-            <h3>${this.precio}</h3>
+            <h3>${this.name}</h3>
+            <h3>${this.amount}</h3>
             </div>
 
             <div class="item-descripcion">
-                <p>${this.descripcion}</p>
+                <p>${this.description}</p>
             </div>
         </div>
 
-    </article>`
-    }
+    </div>`;
+  }
 }
 
-const breakfastUno = new Comida("American pancakes", "$15", "Easy, American-style, fluffy pancakes are great for feeding a crowd at breakfast or brunch. Top with something sweet like fruit, jam or syrup, or rashers of crispy bacon.", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/american-style-pancakes-87119e3.jpg?quality=90&webp=true&resize=300,272");
-const breakfastDos = new Comida("Aire fryer bacon", "$30", "Cook bacon in an air fryer to achieve a crispy texture with less fat. The perfect bacon sandwich starts here", "https://images.immediate.co.uk/production/volatile/sites/30/2022/04/Air-Fryer-Bacon-cc2a084.jpg?quality=90&webp=true&resize=300,272");
-const breakfastTres = new Comida("Prawn & egg on toast", "$30", "Meet your new favourite brunch recipe: quick and easy prawn and egg mayonnaise on toast. Sprinkle with chives to finish", "https://images.immediate.co.uk/production/volatile/sites/30/2022/03/Prawn-and-egg-on-toast-7dda648.jpg?quality=90&webp=true&resize=300,272");
-const breakfastCuatro = new Comida("Smoky beans & baked eggs", "$30", "Pack in the nutrients with smoky beans and baked eggs. Great for a veggie family lunch or supper, serve with flatbreads or toast for extra sustenance", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/smoky-beans-baked-eggs-4a53fb1.jpg?quality=90&webp=true&resize=300,272");
+const breakfastOne = new Food(
+  "American pancakes",
+  "$15",
+  "Easy, American-style, fluffy pancakes are great for feeding a crowd at breakfast or brunch. Top with something sweet like fruit, jam or syrup, or rashers of crispy bacon.",
+  "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/american-style-pancakes-87119e3.jpg?quality=90&webp=true&resize=300,272"
+);
+const breakfastTwo = new Food(
+  "Aire fryer bacon",
+  "$30",
+  "Cook bacon in an air fryer to achieve a crispy texture with less fat. The perfect bacon sandwich starts here",
+  "https://images.immediate.co.uk/production/volatile/sites/30/2022/04/Air-Fryer-Bacon-cc2a084.jpg?quality=90&webp=true&resize=300,272"
+);
+const breakfastTr = new Food(
+  "Prawn & egg on toast",
+  "$30",
+  "Meet your new favourite brunch recipe: quick and easy prawn and egg mayonnaise on toast. Sprinkle with chives to finish",
+  "https://images.immediate.co.uk/production/volatile/sites/30/2022/03/Prawn-and-egg-on-toast-7dda648.jpg?quality=90&webp=true&resize=300,272"
+);
+const breakfastFour = new Food(
+  "Smoky beans & baked eggs",
+  "$30",
+  "Pack in the nutrients with smoky beans and baked eggs. Great for a veggie family lunch or supper, serve with flatbreads or toast for extra sustenance",
+  "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/smoky-beans-baked-eggs-4a53fb1.jpg?quality=90&webp=true&resize=300,272"
+);
 
-let breakfastDesayunos = [breakfastUno, breakfastDos,breakfastTres,breakfastCuatro];
+const breakfasts = [breakfastOne, breakfastTwo, breakfastTr, breakfastFour];
 
-const lunchUno = new Comida("Big-batch bolognese", "$15", "Whip up a huge batch of bolognese that's fit to feed a hungry crowd, or freeze half for a speedy midweek meal", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1074456_10-1a5351d.jpg?quality=90&webp=true&resize=300,272");
-const lunchDos = new Comida("Meal prep: pasta", "$15", "Make three lunchbox pasta meals in one go to save you time midweek. They're nutritious and healthy with variations using salmon, chicken and aubergine", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/mealprep-pasta-merged_web3_copy-8687106.jpg?quality=90&webp=true&resize=300,272");
+const lunchOne = new Food(
+  "Big-batch bolognese",
+  "$15",
+  "Whip up a huge batch of bolognese that's fit to feed a hungry crowd, or freeze half for a speedy midweek meal",
+  "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1074456_10-1a5351d.jpg?quality=90&webp=true&resize=300,272"
+);
+const lunchTwo = new Food(
+  "Meal prep: pasta",
+  "$15",
+  "Make three lunchbox pasta meals in one go to save you time midweek. They're nutritious and healthy with variations using salmon, chicken and aubergine",
+  "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/mealprep-pasta-merged_web3_copy-8687106.jpg?quality=90&webp=true&resize=300,272"
+);
 
-let lunchDesayunos = [lunchUno, lunchDos];
+const lunchs = [lunchOne, lunchTwo];
 
-const shakesUno = new Comida("Breakfast super-shake", "$15", "This smoothie is high in natural fats and sugar - ideal if you need some fuel for intense exercise", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/breakfast-super-shake-e63774c.jpg?quality=90&webp=true&resize=300,272");
-const shakesDos = new Comida("Green spirulina smoothie", "$15", "Start your day with a healthy smoothie. As well as being packed with nutrients, the deep green of spirulina adds rich vibrancy while avocado gives a silky texture", "https://images.immediate.co.uk/production/volatile/sites/30/2022/04/Spirulina-smoothie-40fc97e.jpg?quality=90&webp=true&resize=300,272");
+const shakesOne = new Food(
+  "Breakfast super-shake",
+  "$15",
+  "This smoothie is high in natural fats and sugar - ideal if you need some fuel for intense exercise",
+  "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/breakfast-super-shake-e63774c.jpg?quality=90&webp=true&resize=300,272"
+);
+const shakesTwo = new Food(
+  "Green spirulina smoothie",
+  "$15",
+  "Start your day with a healthy smoothie. As well as being packed with nutrients, the deep green of spirulina adds rich vibrancy while avocado gives a silky texture",
+  "https://images.immediate.co.uk/production/volatile/sites/30/2022/04/Spirulina-smoothie-40fc97e.jpg?quality=90&webp=true&resize=300,272"
+);
 
-let shakesDesayunos = [shakesUno, shakesDos];
+const shakes = [shakesOne, shakesTwo];
 
-let desayunos = [breakfastUno,breakfastDos,breakfastTres,breakfastCuatro, lunchUno,lunchDos, shakesUno, shakesDos];
+const allFoods = [
+  breakfastOne,
+  breakfastTwo,
+  breakfastTr,
+  breakfastFour,
+  lunchOne,
+  lunchTwo,
+  shakesOne,
+  shakesTwo,
+];
 
-window.addEventListener("DOMContentLoaded", ()=>{
+window.addEventListener("DOMContentLoaded", () => {
+  let displayMenu = allFoods.map(function (item) {
+    return item.insertInformation();
+  });
 
-let displayMenu = desayunos.map(function(item){
-    return item.insertarInfo();
-});
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
 
-    displayMenu = displayMenu.join("");
+  btnAll.addEventListener("click", () => {
     sectionCenter.innerHTML = displayMenu;
+  });
 
-    btnAll.addEventListener("click", ()=>{
-        sectionCenter.innerHTML = displayMenu;
+  btnBreakfast.addEventListener("click", () => {
+    let displayMenuBreakfast = breakfasts.map(function (bf) {
+      return bf.insertInformation();
     });
+    displayMenuBreakfast = displayMenuBreakfast.join("");
+    sectionCenter.innerHTML = displayMenuBreakfast;
+  });
 
-    btnBreakfast.addEventListener("click", ()=>{
-        let displayMenuBreakfast = breakfastDesayunos.map(function(bf){
-
-            return bf.insertarInfo();
-        });
-        displayMenuBreakfast = displayMenuBreakfast.join("");
-        sectionCenter.innerHTML = displayMenuBreakfast;
+  btnLunch.addEventListener("click", () => {
+    let displayMenuLunch = lunchs.map(function (lunch) {
+      return lunch.insertInformation();
     });
+    displayMenuLunch = displayMenuLunch.join("");
+    sectionCenter.innerHTML = displayMenuLunch;
+  });
 
-    btnLunch.addEventListener("click", ()=>{
-        let displayMenuLunch = lunchDesayunos.map(function(lunch){
-
-            return lunch.insertarInfo();
-        });
-        displayMenuLunch = displayMenuLunch.join("");
-        sectionCenter.innerHTML = displayMenuLunch;
+  btnShakes.addEventListener("click", () => {
+    let displayMenuShakes = shakes.map(function (shakes) {
+      return shakes.insertInformation();
     });
-
-    btnShakes.addEventListener("click", ()=>{
-        let displayMenuShakes = shakesDesayunos.map(function(shakes){
-
-            return shakes.insertarInfo();
-        });
-        displayMenuShakes = displayMenuShakes.join("");
-        sectionCenter.innerHTML = displayMenuShakes;
-    });
+    displayMenuShakes = displayMenuShakes.join("");
+    sectionCenter.innerHTML = displayMenuShakes;
+  });
 });
-
